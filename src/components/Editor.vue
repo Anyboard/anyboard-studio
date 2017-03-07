dbb2cc4e7dfdaa88e2f2b5a12e0efea90a359042
Sondre <sondrj@stud.ntnu.no>
2017-03-07 15:07:43 +0100

    Can upload images and pick colour for square

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,10 +1,10 @@
 <template>
   <div id = 'app'>
     <canvas id='c' width = '1000' height = '700'></canvas>
-    <input type='button' id='purple' value='Purple square'/>
-    <input type='button' id='yellow' value='Yellow square'/>
-    <input type='button' id='green' value='Green square'/>
+    <input type='button' id='square' value='Square'/>
     <input type='button' id='save' value='Save image'/>
+    <input type='file' id='image' value='Upload image'/>
+    <input type='color' id='picker'/>
   </div>
 </template>
 
@@ -14,6 +14,7 @@
   import FileSaver from 'file-saver'
 
   var canvas
+  var pickedColour = '000000f'
 
   export default {
     name: 'editor',
@@ -26,38 +27,19 @@
       canvas = new fabric.Canvas('c')
       canvas.backgroundColor = 'white'
 
-      // Purple rectangle
-      $('#purple').click(function () {
+      // Set colour
+      $('#picker').change(function () {
+        pickedColour = $('#picker').val()
+      })
+
+      // Square
+      $('#square').click(function () {
         var rect = new fabric.Rect({
           top: 100,
           left: 200,
           width: 100,
           height: 100,
-          fill: 'purple'
-        })
-        canvas.add(rect).setActiveObject(rect)
-      })
-
-      // Yellow rectangle
-      $('#yellow').click(function () {
-        var rect = new fabric.Rect({
-          top: 100,
-          left: 400,
-          width: 100,
-          height: 100,
-          fill: 'yellow'
-        })
-        canvas.add(rect).setActiveObject(rect)
-      })
-
-      // Green rectangle
-      $('#green').click(function () {
-        var rect = new fabric.Rect({
-          top: 100,
-          left: 600,
-          width: 100,
-          height: 100,
-          fill: 'green'
+          fill: pickedColour
         })
         canvas.add(rect).setActiveObject(rect)
       })
@@ -67,6 +49,22 @@
         $('#c').get(0).toBlob(function (blob) {
           FileSaver.saveAs(blob, 'Board.png')
         })
+      })
+
+      // Upload image
+      $('#image').change(function (e) {
+        var file = e.target.files[0]
+        var reader = new FileReader()
+        reader.onload = function (f) {
+          var data = f.target.result
+          fabric.Image.fromURL(data, function (img) {
+            var oImg = img.set({left: 0, top: 0, angle: 0}).scale(0.5)
+            canvas.add(oImg).renderAll()
+            canvas.setActiveObject(oImg)
+            canvas.toDataURL({format: 'png', quality: 0.8})
+          })
+        }
+        reader.readAsDataURL(file)
       })
     }
   }
