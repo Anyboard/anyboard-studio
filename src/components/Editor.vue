0f433796040473bfe1c9895c1bb692db4b35bae0
Sondre <sondrj@stud.ntnu.no>
2017-03-07 12:31:36 +0100

    Separated editor into its own component

---
+++ src/components/Editor.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id = 'app'>
+    <canvas id='c' width = '1000' height = '700'></canvas>
+    <input type='button' id='purple' value='Purple square'/>
+    <input type='button' id='yellow' value='Yellow square'/>
+    <input type='button' id='green' value='Green square'/>
+    <input type='button' id='save' value='Save image'/>
+  </div>
+</template>
+
+<script>
+  import {fabric} from 'fabric'
+  import $ from 'jquery'
+  import FileSaver from 'file-saver'
+
+  var canvas
+
+  export default {
+    name: 'editor',
+    data () {
+      return {
+      }
+    },
+    mounted () {
+      // Setting up Fabric.js canvas
+      canvas = new fabric.Canvas('c')
+      canvas.backgroundColor = 'white'
+
+      // Purple rectangle
+      $('#purple').click(function () {
+        var rect = new fabric.Rect({
+          top: 100,
+          left: 200,
+          width: 100,
+          height: 100,
+          fill: 'purple'
+        })
+        canvas.add(rect).setActiveObject(rect)
+      })
+
+      // Yellow rectangle
+      $('#yellow').click(function () {
+        var rect = new fabric.Rect({
+          top: 100,
+          left: 400,
+          width: 100,
+          height: 100,
+          fill: 'yellow'
+        })
+        canvas.add(rect).setActiveObject(rect)
+      })
+
+      // Green rectangle
+      $('#green').click(function () {
+        var rect = new fabric.Rect({
+          top: 100,
+          left: 600,
+          width: 100,
+          height: 100,
+          fill: 'green'
+        })
+        canvas.add(rect).setActiveObject(rect)
+      })
+
+      // Save button
+      $('#save').click(function () {
+        $('#c').get(0).toBlob(function (blob) {
+          FileSaver.saveAs(blob, 'Board.png')
+        })
+      })
+    }
+  }
+</script>
+
+<style>
+  canvas{
+    border: 1px solid;
+  }
+</style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -17,21 +17,14 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
-    <canvas id='c' width = '1000' height = '700'></canvas>
-    <input type='button' id='purple' value='Purple square'/>
-    <input type='button' id='yellow' value='Yellow square'/>
-    <input type='button' id='green' value='Green square'/>
-    <input type='button' id='save' value='Save image'/>
+    <editor></editor>
   </div>
 </template>
 
 <script>
-import {fabric} from 'fabric'
-import $ from 'jquery'
-import FileSaver from 'file-saver'
-// import editor from '../components/Editor.vue'
-
-var canvas
+import Vue from 'vue'
+import editor from '../components/Editor.vue'
+Vue.component('editor', editor)
 
 export default {
   name: 'hello',
@@ -40,53 +33,8 @@ export default {
       msg: 'Welcome to Your Vue.js App'
     }
   },
-  mounted () {
-    // Setting up Fabric.js canvas
-    canvas = new fabric.Canvas('c')
-    canvas.clear()
-
-    // Purple rectangle
-    $('#purple').click(function () {
-      var rect = new fabric.Rect({
-        top: 100,
-        left: 200,
-        width: 100,
-        height: 100,
-        fill: 'purple'
-      })
-      canvas.add(rect)
-    })
-
-    // Yellow rectangle
-    $('#yellow').click(function () {
-      var rect = new fabric.Rect({
-        top: 100,
-        left: 400,
-        width: 100,
-        height: 100,
-        fill: 'yellow'
-      })
-      canvas.add(rect)
-    })
-
-    // Green rectangle
-    $('#green').click(function () {
-      var rect = new fabric.Rect({
-        top: 100,
-        left: 600,
-        width: 100,
-        height: 100,
-        fill: 'green'
-      })
-      canvas.add(rect)
-    })
-
-    // Save button
-    $('#save').click(function () {
-      $('#c').get(0).toBlob(function (blob) {
-        FileSaver.saveAs(blob, 'Board.png')
-      })
-    })
+  components () {
+    editor
   }
 }
 
@@ -94,25 +42,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 
-canvas{
-  border: 1px solid;
-}
+
+  canvas{
+    border: 1px solid;
+  }
 </style>
