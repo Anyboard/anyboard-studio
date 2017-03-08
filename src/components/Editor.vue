62df5acb3cd50510881cf59b7a2a97abdc857d87
Sondre <sondrj@stud.ntnu.no>
2017-03-08 15:47:05 +0100

    Can print list of tiles and their properties

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,6 +5,7 @@
     <input type='button' id='save' value='Save image'/>
     <input type='file' id='image' value='Upload image'/>
     <input type='color' id='picker'/>
+    <input type='button' id='tiles' value='Tiles'/>
   </div>
 </template>
 
@@ -15,6 +16,7 @@
 
   var canvas
   var pickedColour = '000000f'
+  var tiles = []
 
   export default {
     name: 'editor',
@@ -27,12 +29,12 @@
       canvas = new fabric.Canvas('c')
       canvas.backgroundColor = 'white'
 
-      // Set colour
+      // Set colour when color is picked using standard html5 picker
       $('#picker').change(function () {
         pickedColour = $('#picker').val()
       })
 
-      // Square
+      // Creates a new square/tile and adds it to a list of tiles
       $('#square').click(function () {
         var rect = new fabric.Rect({
           top: 100,
@@ -41,6 +43,7 @@
           height: 100,
           fill: pickedColour
         })
+        tiles.push(rect)
         canvas.add(rect).setActiveObject(rect)
       })
 
@@ -51,7 +54,7 @@
         })
       })
 
-      // Upload image
+      // Upload image, standard boilerplate code
       $('#image').change(function (e) {
         var file = e.target.files[0]
         var reader = new FileReader()
@@ -66,6 +69,15 @@
         }
         reader.readAsDataURL(file)
       })
+
+      // Check current tiles and prints their colours. Can change what it gets to custom properties in the future
+      // Mainly made to show how to get properties of tiles we make
+      $('#tiles').click(function () {
+        console.log(tiles)
+        for (var i = 0; i < tiles.length; i++) {
+          console.log(tiles[i].get('fill'))
+        }
+      })
     }
   }
 </script>
@@ -73,5 +85,14 @@
 <style>
   canvas{
     border: 1px solid;
+  }
+
+  .color label {
+    border-right: solid 1.4em #ddd;
+    width: 6em;
+    display: inline-block;
+    margin: 3px 1em 3px 0;
+    padding: 1px;
+    background: #ddd;
   }
 </style>
