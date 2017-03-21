0624dbc254a7b65470c665b23ea75a25e602cac3
Mjupi <mjupithemop@gmail.com>
2017-03-21 17:45:16 +0100

    CHANGE: Saves tile colours to store

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -5,21 +5,31 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    canvasState: 0
+    canvasState: 0,
+    colours: []
   },
   actions: {
     SAVE_CANVAS: function ({commit}, canvas) {
       commit('SET_CANVAS', canvas)
+    },
+    SAVE_COLOURS: function ({commit}, colours) {
+      commit('SET_COLOURS', colours)
     }
   },
   mutations: {
     SET_CANVAS: (state, canvas) => {
       state.canvasState = canvas
+    },
+    SET_COLOURS: (state, colours) => {
+      state.colours = colours
     }
   },
   getters: {
     GET_CANVAS: state => {
       return state.canvasState
+    },
+    GET_COLOURS: state => {
+      return state.colours
     }
   },
   modules: {
