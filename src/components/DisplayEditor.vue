c0cb4fc9f53fda87c57c7b31f37c70382876287f
Kristian Kanck <kanck039@gmail.com>
2017-04-06 08:37:22 +0200

    laget en 8x8 grid som skifter mellom sort of grønn farge når de klikkes

---
+++ src/components/DisplayEditor.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="displayEditor">
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+    <div class="input-row">
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+      <div class="display-led inactive-led"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import $ from 'jquery'
+
+  export default {
+    name: 'displayEditor'
+  }
+
+  $('.display-led').toggle(function () {
+  },
+  function () {
+
+  })
+
+</script>
+
+<style>
+  #displayEditor{
+    border: 1px solid black;
+    margin: auto;
+  }
+  .display-led{
+    height: 20px;
+    width: 20px;
+
+    border: 2px solid black;
+    border-radius: 50%;
+    margin: 3px;
+
+  }
+  .display-led:hover{
+    cursor: pointer;
+  }
+  .inactive-led{
+    background-color: black;
+  }
+  .active-led{
+    background-color: mediumaquamarine;
+  }
+
+  .input-row{
+    display: flex;
+    flex-direction: column;
+  }
+  #displayEditor{
+    display: flex;
+    flex-direction: row;
+    width: 240px;
+    margin-top: 10px;
+  }
+
+</style>
