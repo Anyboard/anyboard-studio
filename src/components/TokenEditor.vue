d00f1b41508f7589e9a91dd762ec9333866bce86
Kristian Kanck <kanck039@gmail.com>
2017-04-06 08:37:10 +0200

    laget en 8x8 grid som skifter mellom sort of grønn farge når de klikkes

--- src/components/TokenEditor.vue
+++ src/components/TokenEditor.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="tokenEditor">
-    <button>new Token</button>
+    <div id="token-set-up">
+      <label class="firstview-label">Static name:</label><input class="token-input" type="checkbox"><br>
+      <label class="firstview-label">Token name:</label><input class="token-input" type="text">
+
+      <p>Enabled features:</p>
+      <label class="firstview-label">Vibration:</label><input class="token-input" type="checkbox"><br>
+      <label class="firstview-label">Shaking:</label><input class="token-input" type="checkbox"><br>
+      <label class="firstview-label">LED Light:</label><input class="token-input" type="checkbox"><br>
+      <label class="firstview-label">Top display:</label><input class="token-input" type="checkbox"><br>
+      <label class="firstview-label">placeholder event:</label><input class="token-input" type="checkbox"><br>
+      <label class="firstview-label">placeholder event:</label><input class="token-input" type="checkbox"><br>
+      <label class="firstview-label">placeholder event:</label><input class="token-input" type="checkbox"><br>
+    </div>
+
   </div>
 </template>
 
@@ -11,8 +24,22 @@
 </script>
 
 <style>
-#tokenEditor{
-  height: 60vh;
-  border: 1px solid black;
-}
+  #tokenEditor{
+    border: 1px solid black;
+  }
+  .firstview-label{
+    text-align: left;
+    width: 11vw;
+    display: inline-block;
+  }
+  #token-set-up{
+    width: 40vw;
+    margin: auto;
+  }
+  #token-set-up > label{
+    text-align: left;
+  }
+  #token-set-up > input{
+    //text-align: right;
+  }
 </style>
