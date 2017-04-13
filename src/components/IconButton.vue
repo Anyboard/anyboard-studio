ec97cb2b64fc9615a0fd983f5407975bfd0aa65e
Rene Raisanen <rene@raisanen.no>
2017-04-13 15:50:53 +0200

    CHANGE: Wrapping components in design

---
+++ src/components/IconButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+      <span class="icon is-large">
+        <i :class="'fa ' + icon"></i>
+      </span>
+      
+      <span>
+        <b>{{text}}</b>
+        {{tooltip}}
+      </span>
+  </li>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'IconButton',
+    data () { return {} },
+    props: ['icon', 'text', 'tooltip']
+  }
+</script>
+
+<style lang="SASS">
+</style>
