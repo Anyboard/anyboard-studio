11ff9407dfce4d054da6c9e1fd9a22c94843d88c
Kristian Kanck <kanck039@gmail.com>
2017-04-19 11:35:08 +0200

    Functional display editor. User is able to interact with an 8 x 8 grid and produce a 8 x 8 matrix with binary values, where 0 is led off and 1 is led on. when submit-button is pressed, the matrix is printed in console. Need help from René to send the matrix to a global level so that other components can use it. TODO: utilise the matrix to create an svg-map so that the matrix generated corresponds to an image that visually/graphically represents how the matrix will look on the led-display.

--- src/components/DisplayEditor.vue
+++ src/components/DisplayEditor.vue
@@ -1,109 +1,91 @@
 <template>
   <div id="displayEditor">
-    <div class="input-row">
-      <div id="circle1" class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
-    <div class="input-row">
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
-    <div class="input-row">
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
-    <div class="input-row">
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
-    <div class="input-row">
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
-    <div class="input-row">
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
-    <div class="input-row">
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
-    <div class="input-row">
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-      <div class="display-led inactive-led"></div>
-    </div>
+    <div id="led-grid"></div>
+    <button id="submitGrid">Submit</button>
+    <button id="cancelGrid">Cancel</button>
   </div>
 </template>
 
 <script>
+
   import $ from 'jquery'
 
   export default {
-    name: 'displayEditor'
-  }
+    name: 'displayEditor',
+    data () {},
+    mounted () {
+      // Creating grid of led-lights
+      var gridParent = document.getElementById('led-grid')
+      for (var i = 0; i < 8; i++) {
+        var row = document.createElement('div')
+        row.className = 'led-row'
+        row.id = 'row' + i
 
-  $('#circle1').addEventListener('click', function () {
-    if (this.className === 'display-led inactive-led') {
-      this.className = 'display-led active-led'
-    } else {
-      this.className = 'display-led inactive-led'
+        for (var j = 0; j < 8; j++) {
+          var ledLight = document.createElement('div')
+          ledLight.className = 'display-led inactive-led'
+          ledLight.id = 'led' + i + j
+          row.appendChild(ledLight)
+        }
+        gridParent.appendChild(row)
+      }
+
+      // Attaching event listeners to each led light
+      $('.led-row').on('click', '*', function () {
+        if (this.className === 'display-led inactive-led') {
+          this.className = 'display-led active-led'
+        } else {
+          this.className = 'display-led inactive-led'
+        }
+      })
+
+      // create data matrix based on pattern of led-divs.
+
+      function collectionOfData () {
+        $('#submitGrid').on('click', function () {
+          var matrix = [[], [], [], [], [], [], [], []]
+          for (var i = 0; i < 8; i++) {
+            for (var j = 0; j < 8; j++) {
+              var led = document.getElementById('led' + i + j)
+              if (led.className === 'display-led active-led') {
+                matrix[i][j] = 1
+              } else {
+                matrix[i][j] = 0
+              }
+            }
+          }
+          console.log(matrix)
+        })
+      }
+      collectionOfData()
+      /*
+      function collectData () {
+        $('#submitGrid').on('click', '.led-row *', function () {
+          var matrix = [[], [], [], [], [], [], [], []]
+          if (this.className === 'display-led inactive-led') {
+            matrix[this.id.charAt(0)][this.id.charAt(1)] = 0
+          } else {
+            matrix[this.id.charAt(0)][this.id.charAt(1)] = 1
+          }
+        })
+      }
+
+      $('#submitGrid').on('click', function () {
+        console.log(collectData())
+      })
+      */
     }
-  })
-
+  }
 </script>
 
 <style>
-  #displayEditor{
+  #led-grid{
     border: 1px solid black;
     margin: auto;
+    display: flex;
+    flex-direction: column;
+    width: 240px;
+    margin-top: 10px;
   }
   .display-led{
     height: 20px;
@@ -116,6 +98,7 @@
   }
   .display-led:hover{
     cursor: pointer;
+    background-color: mediumaquamarine;
   }
   .inactive-led{
     background-color: black;
@@ -124,15 +107,9 @@
     background-color: mediumaquamarine;
   }
 
-  .input-row{
-    display: flex;
-    flex-direction: column;
-  }
-  #displayEditor{
+  .led-row{
     display: flex;
     flex-direction: row;
-    width: 240px;
-    margin-top: 10px;
   }
 
 </style>
