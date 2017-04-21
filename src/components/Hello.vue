671e0357fa72962130786280e024ffaade0aefbf
Magda Angelica <andreas@williams.no>
2017-04-21 16:01:15 +0100

    Added brief description of each tab on the front page

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,51 +5,26 @@
       <p>This is a simple web application for building custom hybrid board games. It utilises Evothings to run the games and we use the Fabric.js and Blockly.js libs for building the games.</p>
 
       <p>Head over to the <a>docs</a> for information on how to get started on your fully personal game.</p>
-      
+
+      <h2>How to get started</h2>
+      <h3> Board editor </h3>
       <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum enim mauris. Suspendisse posuere commodo erat, sit amet mollis neque molestie condimentum. Curabitur placerat convallis feugiat. Nullam orci quam, porttitor vitae tempor ultrices, fringilla id massa. Fusce non nibh sit amet turpis mattis pretium vitae non mauris. Praesent ornare tellus at iaculis rhoncus. Vivamus consectetur sodales justo non vestibulum. Maecenas diam ligula, tincidunt sit amet ex nec, placerat sollicitudin mi. Quisque sit amet lectus id nibh tempus lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus cursus dui augue, sit amet semper metus blandit in. Vestibulum faucibus sapien vel varius sodales. In condimentum at diam in interdum. Pellentesque non ligula massa. Nullam a vestibulum orci, quis euismod ligula.
+      This is where you will design the board that your game will use. Use the toolbar on the left side to select different sectors. The sectors can have different colors, sizes and shapes. You can manipulate the sectors in different ways by using the different tools in the toolbar. You can also upload images, free draw and toggle the grid function on and off. Hover the mouse over the different icons for a brief explanation of the function.
+      The color of a sector represents a certain event that will be triggered when a pawn is on the sector. E.g, when a pawn is on a sector colored red, the player might lose a point and have the grid display a X. The logic for the events that are triggered when a pawn lands on a certain sector is defined in the code builder.
+      When you are done editing your game board you can print it by downloading it as a image file by clicking on the download button in the toolbar.
       </p>
+      <h3>Game assets</h3>
       <p>
-      Vivamus eget ligula ac nulla porta imperdiet. Sed id suscipit ipsum, lacinia ultricies nisl. Duis orci neque, bibendum eu euismod id, tempus quis nibh. Mauris metus nibh, feugiat egestas dolor maximus, tincidunt tristique tellus. Sed fermentum ex quis velit imperdiet malesuada. Morbi rutrum aliquam fermentum. Nam eget sagittis dui.
+      This tab contains various functions for editing the cards printed by the Anydeck, a list of available token grids and a mode for customising your own grids. You can also edit various tokens properties, such as name and valid actions.
       </p>
+      <h3>Code builder</h3>
       <p>
-      Nunc pharetra ornare libero in lobortis. Morbi convallis turpis eu quam feugiat auctor vitae id augue. Maecenas tristique facilisis ex ut porta. Donec lacinia dapibus metus, eget mollis nisi semper et. Morbi in orci imperdiet, mattis ligula eu, euismod eros. Etiam nec volutpat nunc. Etiam eu dolor magna. Curabitur in odio quis ligula dapibus vehicula. Cras massa ex, semper non vehicula nec, egestas at eros. Sed posuere nunc eget feugiat porttitor. Pellentesque mauris mauris, vulputate eu tincidunt ac, lobortis in orci.
+      This is where you define the logic of your game. The code is represented by blocks, and it’s your job to put these blocks together however you may please to create the logic for your gamerules. The different parts of the logic are sorted in different categories to the left.  For example, if you want a certain token to vibrate on a certain sector, your code block may look something like this:
+      When you are done making the logic for your game, hit the export button in the top right corner. This will you give a HTML file that contains the code for your game. Place the HTML file inside the game folder.
       </p>
+      <h3>EvoThings</h3>
       <p>
-      Etiam pretium varius urna, congue molestie eros ultricies venenatis. Duis et mi luctus, cursus sem eget, aliquet dolor. Integer vestibulum sit amet leo placerat varius. Phasellus porta purus magna, a blandit velit efficitur eget. Aenean vehicula ex id velit bibendum convallis. Mauris accumsan, enim sit amet rhoncus pharetra, quam neque cursus lacus, et hendrerit quam ex eget sapien. Quisque dictum eget lorem tristique rutrum. Etiam nec odio vel leo viverra tempor. Vivamus neque neque, aliquet id eros eu, faucibus volutpat felis. Nulla lobortis porta sapien nec mollis. Nullam id risus est. Vestibulum laoreet rutrum porta.
-      </p>
-      <p>
-      Etiam semper, neque id sagittis varius, ligula ipsum scelerisque nunc, feugiat blandit enim nisl ut magna. Etiam eget est in nisi viverra imperdiet. Etiam porttitor enim sed dapibus egestas. Sed placerat auctor ex, vitae elementum nisl sollicitudin eu. Etiam vel mauris quam. Curabitur vitae velit semper, dictum ipsum pellentesque, elementum nulla. Nam mauris quam, sollicitudin sed rhoncus eu, suscipit id nunc. Phasellus tincidunt ac sapien et iaculis. Cras commodo interdum accumsan. Aenean laoreet orci vitae sem lacinia semper. Pellentesque hendrerit, dolor in consequat ornare, ex ante varius nunc, non semper metus tellus bibendum arcu. Pellentesque a mauris et erat consectetur interdum at non ligula.
-      </p>
-      <p>
-      Maecenas a elit nisi. Nunc volutpat iaculis lorem eu convallis. Proin non risus in mi efficitur facilisis. Maecenas eget ante eu augue ultricies posuere eu nec tellus. Maecenas vehicula dolor eleifend imperdiet suscipit. Curabitur bibendum commodo ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at mauris auctor, dapibus augue vitae, pretium tellus. Phasellus erat lacus, suscipit a vulputate non, volutpat eu enim. Mauris consequat sem ut turpis elementum convallis. Morbi malesuada porttitor elementum.
-      </p>
-      <p>
-      Phasellus lacus massa, faucibus et laoreet vel, tristique ac dui. Donec id neque ante. Aliquam id consequat justo. Aliquam ornare tincidunt condimentum. Vivamus nec felis ac leo bibendum gravida gravida quis ex. Pellentesque id mauris libero. Sed elementum, sem sed efficitur volutpat, massa erat aliquet odio, ac condimentum lorem dui ut purus. Nulla faucibus libero sed fringilla dignissim. Vestibulum pharetra ex eu odio dapibus, a rhoncus eros cursus. Quisque pretium ac ligula sed ornare.
-      </p>
-      <p>
-      Donec nulla ex, dapibus at aliquam nec, ultricies nec nibh. In ullamcorper lacus vitae tincidunt ullamcorper. Sed convallis leo eu diam suscipit, non ullamcorper urna convallis. Donec velit nisl, lacinia sit amet purus et, dictum scelerisque ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus at elit efficitur, pretium nunc nec, efficitur ligula. Pellentesque id dolor risus. Sed ultrices bibendum sapien, sit amet ultrices ex eleifend a. Etiam hendrerit euismod vestibulum. Donec gravida sit amet est fringilla malesuada.
-      </p>
-      <p>
-      Curabitur tristique sollicitudin sollicitudin. Suspendisse potenti. Ut et finibus magna. Maecenas nisl ligula, commodo eget sapien id, sagittis dignissim felis. Etiam sem mi, dignissim ut felis ut, luctus ultrices quam. Cras non facilisis lorem. Suspendisse gravida libero eget tortor gravida auctor. Donec quis nisl at turpis vulputate elementum.
-      </p>
-      <p>
-      Proin nulla arcu, efficitur a congue id, convallis ut ligula. Etiam consequat elit eget metus elementum, a auctor enim bibendum. Phasellus interdum laoreet libero, non maximus velit facilisis ut. Pellentesque et mauris at ipsum lacinia vestibulum vitae ac nibh. Nullam sit amet egestas quam, nec suscipit augue. Sed finibus egestas blandit. Praesent lacinia imperdiet sem, ac tincidunt purus vehicula sed. Aenean tempor faucibus ipsum, ac placerat est luctus non. Suspendisse potenti. Mauris eget pharetra sem. Aliquam at varius magna. Ut sollicitudin auctor justo, quis cursus lectus tristique eu. Fusce aliquet venenatis ante.
-      </p>
-      <p>
-      Duis eu lacus eu lacus aliquam rhoncus quis vel erat. Aenean malesuada eget neque eu ultricies. Proin quis felis porta, venenatis enim eu, eleifend diam. Duis consequat enim nec dolor placerat, eu dictum lectus dictum. Etiam semper tortor mi, in congue enim faucibus fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu neque vel lorem feugiat pulvinar. Morbi pharetra sapien in mauris eleifend posuere. Nullam a dictum ipsum. Quisque ut odio velit.
-      </p>
-      <p>
-      Donec eu odio at elit venenatis tincidunt et id nulla. Maecenas vestibulum lacinia interdum. Nunc tortor dui, tempor vitae lectus vel, commodo condimentum est. Etiam ut quam euismod, fermentum nibh at, finibus lorem. Morbi nec est mauris. Nullam sit amet urna efficitur, sodales risus at, semper tellus. Quisque cursus neque eu nulla placerat, eget vehicula nunc faucibus. Vestibulum fermentum blandit ipsum vitae blandit. Fusce nibh magna, imperdiet eu pretium sit amet, semper vitae justo.
-      </p>
-      <p>
-      Vestibulum nisi ipsum, tincidunt vel elementum id, tempus eu nisl. Etiam consequat quis ipsum a sagittis. Sed in sagittis lectus, a tempus velit. Sed blandit congue ipsum at fermentum. Donec ornare pharetra est at finibus. Maecenas malesuada urna in posuere fermentum. Nulla vel laoreet mi. Etiam venenatis congue porttitor. In id gravida lorem, eget dignissim sapien. Mauris sed massa eros. Donec id semper nibh, vitae interdum eros. Nunc lectus lorem, cursus vel lectus sit amet, volutpat fringilla mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-      </p>
-      <p>
-      Nam convallis dapibus augue, sagittis pulvinar dui commodo at. Maecenas vestibulum viverra tortor, id placerat magna efficitur et. Donec mollis pellentesque mi ac accumsan. Morbi ut purus est. Suspendisse potenti. Aenean eget neque at ligula fringilla sagittis et id dolor. Morbi ac erat ligula. Duis eget mi lorem.
-      </p>
-      <p>
-      Nullam pellentesque ligula in est tempus aliquet. Curabitur dignissim elit id suscipit lacinia. Vestibulum tincidunt suscipit neque ac dignissim. Cras fringilla diam leo, eu scelerisque felis feugiat eget. Cras molestie pharetra vehicula. Sed scelerisque, nibh sit amet malesuada pretium, leo tortor consectetur sem, at faucibus enim ante vitae erat. Vivamus at cursus ex. Vivamus gravida ipsum vitae nunc pretium, at mollis ligula tempor. Maecenas nisi quam, viverra in enim vitae, pharetra aliquet justo. Curabitur cursus condimentum mauris, accumsan ornare felis mollis a. Mauris porta, risus ut ultricies venenatis, enim est eleifend augue, sed imperdiet ipsum turpis ut risus.
+      Open EvoThings on the computer and click the Connect tab. Click on the Get Key button to generate a key. Open up the EvoThings application on the smart phone and type in the key. Click connect and wait for a screen that says “Connected”. Go back in to EvoThings on the computer and click on the My Apps tab. Locate the game in the list called AnyboardGameDemo and click on Run. The game is now running on the smartphone in the EvoThings application. Click on Discover bluetooth devices and select the Anypawn. The Anypawn is now connected to the phone which is running the code you created with the code builder.
       </p>
     </div>
   </div>
