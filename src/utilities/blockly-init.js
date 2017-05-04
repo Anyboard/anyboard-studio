75642152d86decf1b93cf05300213bcd18733ff8
Felix Prinz <felix@prinz.no>
2017-05-04 15:39:33 +0200

    bug fixing: functions that take in arguments now work

--- src/utilities/blockly-init.js
+++ src/utilities/blockly-init.js
@@ -955,7 +955,7 @@ const blocklyInit = function (Blockly, TOKENS, GRIDS, sectorObject, sectorNames,
     }
     var args = ['currentToken']
     for (var i = 1; i <= block.arguments_.length; i++) {
-      args[i] = Blockly.JavaScript.variableDB_.getName(block.arguments_[i],
+      args[i] = Blockly.JavaScript.variableDB_.getName(block.arguments_[i - 1],
           Blockly.Variables.NAME_TYPE)
     }
     var code = funcName + ': function (' + args.join(', ') + ') {\n' +
