8760860307e129418028a44b42a712acf7c3ec9e
Rene Raisanen <rene@raisanen.no>
2017-05-15 02:28:36 +0200

    Preparing for moving Beta -> Master

--- src/utilities/htmlConverter.py
+++
@@ -1,9 +0,0 @@
-with open("../anyboardGameDemo/index.html", "r") as xml_file:
-    lines = xml_file.read().splitlines()
-file = open("templateHTML.txt","w")
-
-for line in lines:
-    line = line.replace("'", "\\'")
-    file.write("htmlTemplate += '" + line + "\\n'\n")
-
-file.close()
